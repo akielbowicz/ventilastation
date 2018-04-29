fb23c63a9facf88e6ad7535eb6bd93049f419b56
Ezequiel Bernatene <ebernatene@gmail.com>
2018-04-29 14:52:19 -0300

    Armamos un test, con file corazones.png. Lo generalizamos para cualquier imagen

---
+++ test_to_led.py
@@ -0,0 +1,3 @@
+from to_led import to_led
+
+print(to_led(path='./files/corazon.png'))
--- to_led.py
+++ to_led.py
@@ -2,15 +2,27 @@ import matplotlib.image as mpimg
 from numpy import sin, cos, pi
 from struct import pack
 
-def to_led(path=None):
-    img = mpimg.imread(path)
+"""
+La funcion toma un archivo *.png y devuelve en binario (RGBI) la secuencia 
+de n_leds prendidos para n_ang diferentes
+"""
+
+def to_led(path=None, n_led=50, n_ang = 32):
+    
+    img = mpimg.imread(path)        # Levanta la imagen
+
+    wx, wy, dim = img.shape         # Me da las dimensiones del frame en pixeles
+    center_x = int((wx-1)/2)        # Calcula la cordenada x del centro de la imagen
+    center_y = int((wy-1)/2)        # Calcula la cordenada y del centro de la imagen
+    rad = min(center_x, center_y)   # Calcula el radio que barre la imagen dentro del frame
 
     led = []
-
-    for m in range(0,32):
-        for n in range(1,51):
-            x = 511 + int(511 * n/50 * cos(m * pi/16))
-            y = 561 + int( 511 * n/50 * sin(m * pi/16))
+    
+    for m in range(0,n_ang):
+        for n in range(0,n_led):
+            x = center_x + int(rad * (n+1)/n_led * cos(m * 2*pi/n_ang)) 
+            y = center_y + int(rad * (n+1)/n_led * sin(m * 2*pi/n_ang))
+            print(x,y)
             tupla = tuple(int(g) for g in img[x,y])
             led.append(pack('BBBB', *tupla))
-    return led
+    return b''.join(led)
