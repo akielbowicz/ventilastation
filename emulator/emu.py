8dada1ac552cffe2ac7a5b368b0773666c0725ae
Manuel Quiñones <manuel.por.aca@gmail.com>
2018-04-29 23:59:22 -0300

    emu refactor

--- emulator/emu.py
+++ emulator/emu.py
@@ -7,17 +7,19 @@ LED_COUNT = 50
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 
+def line_data_to_rgba(data):
+    line_bytes = bytearray(data)
+    bytes_list = (line_bytes[i:i+4] for i  in range(0, len(line_bytes), 4))
+    rgba_list = (unpack('BBBB', byte) for byte in bytes_list)
+    return rgba_list
+
 def file_iterator(filename):
     with open(filename, 'rb') as f:
         while True:
             data = f.read(4*LED_COUNT)
             while data != b"":
-                line_bytes = bytearray(data)
-                bytes_list = (line_bytes[i:i+4] for i  in range(0, len(line_bytes), 4))
-                rgba_list = (unpack('BBBB', byte) for byte in bytes_list)
-                yield rgba_list
+                yield line_data_to_rgba(data)
                 data = f.read(4*LED_COUNT)
-
             f.seek(0)
 
 def sock_iterator():
@@ -26,10 +28,7 @@ def sock_iterator():
     sock.bind((UDP_IP, UDP_PORT))
     while True:
         data, _ = sock.recvfrom(1024)
-        line_bytes = bytearray(data)
-        bytes_list = (line_bytes[i:i+4] for i  in range(0, len(line_bytes), 4))
-        rgba_list = (unpack('BBBB', byte) for byte in bytes_list)
-        yield rgba_list
+        yield line_data_to_rgba(data)
 
 
 PygletEngine(50, 128, file_iterator('../files/corgibus128.bytes'))
