e54ad97eab0087eb8a73a106b0100d27bcb77002
Ezequiel Bernatene <ebernatene@gmail.com>
2018-04-29 23:49:08 -0300

    Modifico para que guarde el gif.bytes en unico archivo

--- from_gif_to_bytes.py
+++ from_gif_to_bytes.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageSequence
 from to_led import to_led
 from pathlib import Path
 
-def from_gif_to_bytes(file_path):
+def from_gif_to_bytes(file_path, n_led=50, n_ang=32):
     "Crea archivos .bytes a partir de un .gif"
     file_path = Path(file_path)
     input_file_name = file_path.name.split('.')[0]
@@ -16,11 +16,10 @@ def from_gif_to_bytes(file_path):
 
     output_folder_name = str(picture_folder)
 
-    for index,frame in enumerate(ImageSequence.Iterator(im)):
-        file_name = 'frame{:d}'.format(index)
-        temp_file = ''.join([temp_path, '/', file_name, '.png'])
-        frame.save(temp_file)
-        out_file = ''.join([output_folder_name, '/', file_name, '.bytes'])
-        # print(index, file_name, temp_file, out_file,sep='\n')
-        with open(out_file, 'wb') as f:
-            f.write(to_led(temp_file))
+    out_file = ''.join([output_folder_name, '/', input_file_name, '.bytes'])
+    with open(out_file, 'wb') as f:
+        for index,frame in enumerate(ImageSequence.Iterator(im)):
+            file_name = 'frame{:d}'.format(index)
+            temp_file = ''.join([temp_path, '/', file_name, '.png'])
+            frame.save(temp_file)
+            f.write(to_led(temp_file, n_led, n_ang))
