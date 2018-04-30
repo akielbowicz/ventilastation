6bc688c5805455734b2eb9f7fdbc6fad853a672d
Ezequiel Bernatene <ebernatene@gmail.com>
2018-04-29 22:14:54 -0300

    Agrego funcion que toma gifs y devuelve sus .bytes

---
+++ from_gif_to_bytes.py
@@ -0,0 +1,26 @@
+import tempfile
+from PIL import Image, ImageSequence
+from to_led import to_led
+from pathlib import Path
+
+def from_gif_to_bytes(file_path):
+    "Crea archivos .bytes a partir de un .gif"
+    file_path = Path(file_path)
+    input_file_name = file_path.name.split('.')[0]
+
+    picture_folder = file_path.parent.joinpath(input_file_name)
+    picture_folder.mkdir(exist_ok=True)
+
+    im = Image.open(file_path)
+    temp_path = tempfile.mkdtemp()
+
+    output_folder_name = str(picture_folder)
+
+    for index,frame in enumerate(ImageSequence.Iterator(im)):
+        file_name = 'frame{:d}'.format(index)
+        temp_file = ''.join([temp_path, '/', file_name, '.png'])
+        frame.save(temp_file)
+        out_file = ''.join([output_folder_name, '/', file_name, '.bytes'])
+        print(index, file_name, temp_file, out_file,sep='\n')
+        with open(out_file, 'wb') as f:
+            f.write(to_led(temp_file))
--- test_to_led.py
+++ test_to_led.py
@@ -1,9 +1,4 @@
 import sys
 from to_led import to_led
 
-<<<<<<< HEAD
-
-sys.stdout.buffer.write(to_led(path='./files/mario.png',n_ang=128))
-=======
 sys.stdout.buffer.write(to_led(path='./files/corazon.png'))
->>>>>>> 94a1b79d5a0bf8c3e721f2a8ce39b6594f8a442f
